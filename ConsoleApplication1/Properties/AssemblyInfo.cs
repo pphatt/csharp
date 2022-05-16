02840c381b8c5f3ab5f53ff6488e96208d79a4c8
phatshibukai <vutienphat0809@gmail.com>
2022-05-16 22:58:22 +0700

    Clean commit

--- ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("290D791C-5490-47DE-8DC0-63E2AFFF37C7")]
+[assembly: Guid("14BBCEEA-F7B3-41C1-B3B6-BA7FB30F721C")]
 
 // Version information for an assembly consists of the following four values:
 //

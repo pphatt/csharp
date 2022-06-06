cca4fc924d70f990ab953c9e4d772b9d6419ca8a
phatshibukai <vutienphat0809@gmail.com>
2022-06-06 18:59:24 +0700

    Update

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,7 +8,19 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            string id = "", name = "", age = "", classroom = "";
+            string id = "",
+                name = "",
+                age = "",
+                classroom = "",
+                Math_grade = "",
+                Physics_grade = "",
+                Chemistry_grade = "",
+                Literature_grade = "",
+                English_grade = "",
+                History_grade = "",
+                totalGrade = "";
+            int tmpTotalGrade = 0;
+
             List<List<List<string>>> data = new List<List<List<string>>>();
             int checkEd = 0;
             while (true)
@@ -36,8 +48,9 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
+
                         currentStudentData.Add(id);
-                        
+
                         Console.Write("Input student name: ");
                         name = Console.ReadLine();
                         if (name.Length == 0)
@@ -46,17 +59,17 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-                        
+
                         string[] name1 = name.Split(' ');
                         name1 = name1.Where(x => x != "").ToArray();
                         for (int i = 0; i < name1.Length; i++)
                         {
                             name1[i] = name1[i].First().ToString().ToUpper() + name1[i].Substring(1);
                         }
-                        
+
                         name = string.Join(" ", name1);
                         currentStudentData.Add(name);
-                        
+
                         Console.Write("Input student age: ");
                         age = Console.ReadLine();
                         if (age.Length == 0)
@@ -65,8 +78,9 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
+
                         currentStudentData.Add(age);
-                        
+
                         Console.Write("Input student class: ");
                         classroom = Console.ReadLine();
                         if (classroom.Length == 0)
@@ -75,36 +89,63 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-            
+
                         classroom = classroom.First().ToString().ToUpper() + classroom.Substring(1);
 
                         bool check = false;
-                        
+
                         for (int i = 0; i < data.Count; i++)
                         {
                             if (classroom == data[i][0][3])
                             {
                                 currentStudentData.Add(classroom);
                                 data[i].Add(currentStudentData);
-                                Console.WriteLine("-----------------------------");
-                                Console.WriteLine("---- Update Successfully ----");
-                                Console.WriteLine("-----------------------------");
+                                // Console.WriteLine("-----------------------------");
+                                // Console.WriteLine("---- Update Successfully ----");
+                                // Console.WriteLine("-----------------------------");
                                 check = true;
                                 break;
                             }
                         }
-                        
+
                         if (check == false)
                         {
                             currentStudentData.Add(classroom);
                             currentClass.Add(currentStudentData);
                             data.Add(currentClass);
                         }
+
+                        Console.Write("Input Math grade: ");
+                        Math_grade = Console.ReadLine();
+                        currentStudentData.Add(Math_grade);
+                        Console.Write("Input Physics grade: ");
+                        Physics_grade = Console.ReadLine();
+                        currentStudentData.Add(Physics_grade);
+                        Console.Write("Input Chemistry grade: ");
+                        Chemistry_grade = Console.ReadLine();
+                        currentStudentData.Add(Chemistry_grade);
+                        Console.Write("Input Literature grade: ");
+                        Literature_grade = Console.ReadLine();
+                        currentStudentData.Add(Literature_grade);
+                        Console.Write("Input English grade: ");
+                        English_grade = Console.ReadLine();
+                        currentStudentData.Add(English_grade);
+                        Console.Write("Input History grade: ");
+                        History_grade = Console.ReadLine();
+                        currentStudentData.Add(History_grade);
+
+                        tmpTotalGrade = Int32.Parse(Math_grade) + Int32.Parse(Physics_grade) +
+                                        Int32.Parse(Chemistry_grade) + Int32.Parse(Literature_grade) + Int32.Parse(History_grade) + Int32.Parse(English_grade);
+
+                        tmpTotalGrade = tmpTotalGrade / 6;
                         
+                        totalGrade = $"{tmpTotalGrade}";
+                        currentStudentData.Add(totalGrade);
+
                         Console.WriteLine("-----------------------------");
                         Console.WriteLine("---- Update Successfully ----");
                         Console.WriteLine("-----------------------------");
-                        
+
                         break;
                     }
                     else if (number == 2)
@@ -116,15 +157,15 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-                        
+
                         for (int i = 0; i < data.Count; i++)
                         {
                             Console.WriteLine($"-> {i + 1}. {data[i][0][3]}");
                         }
-            
+
                         Console.Write("Input to use: ");
                         int number1 = int.Parse(Console.ReadLine());
-                        
+
                         if (number1 >= 1 && number1 <= data.Count)
                         {
                             Console.WriteLine("-----------------------------");
@@ -136,6 +177,7 @@ namespace ConsoleApplication1
                             {
                                 Console.WriteLine($"There are {data[number1 - 1].Count} students in the class");
                             }
+
                             for (int i = 0; i < data[number1 - 1].Count; i++)
                             {
                                 Console.WriteLine("-----------------------------");
@@ -144,11 +186,39 @@ namespace ConsoleApplication1
                                 Console.WriteLine($"-> Name: {data[number1 - 1][i][1]}");
                                 Console.WriteLine($"-> Age: {data[number1 - 1][i][2]}");
                                 Console.WriteLine($"-> Class: {data[number1 - 1][i][3]}");
+                                Console.WriteLine($"-> Math grade: {data[number1 - 1][i][4]}");
+                                Console.WriteLine($"-> Physics grade: {data[number1 - 1][i][5]}");
+                                Console.WriteLine($"-> Chemistry grade: {data[number1 - 1][i][6]}");
+                                Console.WriteLine($"-> Literature grade: {data[number1 - 1][i][7]}");
+                                Console.WriteLine($"-> English grade: {data[number1 - 1][i][8]}");
+                                Console.WriteLine($"-> History grade: {data[number1 - 1][i][9]}");
+                                Console.WriteLine($"-> Total grade: {data[number1 - 1][i][10]}");
+                                // Console.WriteLine("-----------------------------");
+                                
+                                if (Int32.Parse(data[number1 - 1][i][10]) >= 5 && Int32.Parse(data[number1 - 1][i][10]) <= 7)
+                                {
+                                    Console.WriteLine($"Pass with the score: {data[number1 - 1][i][10]} -> (P)");
+                                }
+                                else if (Int32.Parse(data[number1 - 1][i][10]) > 7 && Int32.Parse(data[number1 - 1][i][10]) <= 8.5)
+                                {
+                                    Console.WriteLine($"Pass with the score: {data[number1 - 1][i][10]} -> (M)");
+                                }
+                                else if (Int32.Parse(data[number1 - 1][i][10]) > 8.5)
+                                {
+                                    Console.WriteLine($"Pass with the score: {data[number1 - 1][i][10]} -> (D)");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Failed with the score: {data[number1 - 1][i][10]} -> (F)");
+                                }
+
                                 Console.WriteLine("-----------------------------");
                             }
+
                             Console.ReadKey();
                             break;
                         }
+
                         Console.WriteLine("Invalid Number");
                         break;
                     }
@@ -157,6 +227,7 @@ namespace ConsoleApplication1
                         checkEd = 3;
                         break;
                     }
+
                     Console.WriteLine("Invalid number");
                 }
 

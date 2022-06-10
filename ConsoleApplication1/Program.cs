2819fbd7b947a7c20f4968392b62c16319cfe9a6
phatshibukai <vutienphat0809@gmail.com>
2022-06-10 20:22:11 +0700

    Update: Comment

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,8 +8,17 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
+            // Making Three dimensional List for storing data.
+            // Why is it 3D array ? -> Because we are making a program to store students data and sorted by each classes.
+            // First layer of array is like the whole school.
+            // Second layer is multiple classes in it.
+            // Third layer is multiple students data in specific class.
             List<List<List<string>>> data = new List<List<List<string>>>();
+
+            // checkEd variable here is for the exit.
             int checkEd = 0;
+
+            // We are using while true for making the menu.
             while (true)
             {
                 Console.WriteLine("*****************************");
@@ -23,12 +32,21 @@ namespace ConsoleApplication1
                 Console.WriteLine("*****************************");
                 while (true)
                 {
+                    // Making two List:
+                    // One is a normal List for storing current student data when user input.
+                    // Two is a two dimensional List for current student class.
+                    // Why is it 2D array ? -> Because we are making a place to store multiple students data not just a single student and 2D can help to solve that problem.
                     List<string> currentStudentData = new List<string>();
                     List<List<string>> currentClass = new List<List<string>>();
+                    
+                    // Accepting the user input.
                     Console.Write("Input to use: ");
                     int number = int.Parse(Console.ReadLine());
+                    
+                    // Checking if the user input is number 1 or not.
                     if (number == 1)
                     {
+                        // We get student data like: ID, Name, Age, Class, and Grade of some Subjects
                         Console.WriteLine("-----------------------------");
                         Console.Write("Input student ID: ");
                         string id = Console.ReadLine();
@@ -49,15 +67,22 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-
-                        string[] name1 = name.Split(' ');
-                        name1 = name1.Where(x => x != "").ToArray();
-                        for (int i = 0; i < name1.Length; i++)
+                        
+                        // In here, we want name get capitalized in each specific name.
+                        // So we butcher the name by Split(). (1)
+                        // And we looking for if there is any unwanted-white-space in out array and remove it. (2)
+                        // Next, we capitalize all the name and concat the name together. (3)
+                        
+                        string[] name1 = name.Split(' '); // (1)
+                        
+                        name1 = name1.Where(x => x != "").ToArray(); // (2)
+                        
+                        for (int i = 0; i < name1.Length; i++) // (3)
                         {
                             name1[i] = name1[i].First().ToString().ToUpper() + name1[i].Substring(1);
                         }
 
-                        name = string.Join(" ", name1);
+                        name = string.Join(" ", name1); // (3)
                         currentStudentData.Add(name);
 
                         Console.Write("Input student age: ");
@@ -79,14 +104,18 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-
+                        
+                        // We capitalize the first character in the class name.
                         classroom = classroom.First().ToString().ToUpper() + classroom.Substring(1);
-
-                        bool check = false;
+                        
+                        // the check variable here is for checking if the class is already exist or not. (1)
+                        // if the class is exist, we just add in the current class and we don't need the making the new ones. (2)
+                        // And if not we make a new class. (3)
+                        bool check = false; // (1)
 
                         for (int i = 0; i < data.Count; i++)
                         {
-                            if (classroom == data[i][0][3])
+                            if (classroom == data[i][0][3]) // (2)
                             {
                                 currentStudentData.Add(classroom);
                                 data[i].Add(currentStudentData);
@@ -95,39 +124,46 @@ namespace ConsoleApplication1
                             }
                         }
 
-                        if (check == false)
+                        if (check == false) // (3)
                         {
                             currentStudentData.Add(classroom);
                             currentClass.Add(currentStudentData);
                             data.Add(currentClass);
                         }
-
+                        
+                        // Here we get all the subjects grade.
                         Console.Write("Input Math grade: ");
                         string Math_grade = Console.ReadLine();
                         currentStudentData.Add(Math_grade);
+                        
                         Console.Write("Input Physics grade: ");
                         string Physics_grade = Console.ReadLine();
                         currentStudentData.Add(Physics_grade);
+                        
                         Console.Write("Input Chemistry grade: ");
                         string Chemistry_grade = Console.ReadLine();
                         currentStudentData.Add(Chemistry_grade);
+                        
                         Console.Write("Input Literature grade: ");
                         string Literature_grade = Console.ReadLine();
                         currentStudentData.Add(Literature_grade);
+                        
                         Console.Write("Input English grade: ");
                         string English_grade = Console.ReadLine();
                         currentStudentData.Add(English_grade);
+                        
                         Console.Write("Input History grade: ");
                         string History_grade = Console.ReadLine();
                         currentStudentData.Add(History_grade);
-
+                        
+                        // We calculate the total grade to calculate the status of the student. Is that student pass or failed or something based on the student's status. (1)
                         Double tmpTotalGrade = Double.Parse(Math_grade) + Double.Parse(Physics_grade) +
                                                Double.Parse(Chemistry_grade) + Double.Parse(Literature_grade) +
-                                               Double.Parse(History_grade) + Double.Parse(English_grade);
+                                               Double.Parse(History_grade) + Double.Parse(English_grade); // (1)
 
-                        tmpTotalGrade = tmpTotalGrade / 6;
+                        tmpTotalGrade = tmpTotalGrade / 6; // (1)
 
-                        string totalGrade = $"{tmpTotalGrade:F1}";
+                        string totalGrade = $"{tmpTotalGrade:F1}"; // (1)
                         currentStudentData.Add(totalGrade);
 
                         Console.WriteLine("-----------------------------");
@@ -138,6 +174,7 @@ namespace ConsoleApplication1
                     }
                     else if (number == 2)
                     {
+                        // So option 2 is that show the student's data in the database.
                         Console.WriteLine("-----------------------------");
                         if (data.Count == 0)
                         {
@@ -145,7 +182,8 @@ namespace ConsoleApplication1
                             Console.WriteLine("-----------------------------");
                             break;
                         }
-
+                        
+                        // In here, we display any available class in the database.
                         for (int i = 0; i < data.Count; i++)
                         {
                             Console.WriteLine($"-> {i + 1}. {data[i][0][3]}");
@@ -153,7 +191,8 @@ namespace ConsoleApplication1
 
                         Console.Write("Input to use: ");
                         int number1 = int.Parse(Console.ReadLine());
-
+                        
+                        // Showing student's data based on user option.
                         if (number1 >= 1 && number1 <= data.Count)
                         {
                             Console.WriteLine("-----------------------------");
@@ -165,7 +204,8 @@ namespace ConsoleApplication1
                             {
                                 Console.WriteLine($"There are {data[number1 - 1].Count} students in the class");
                             }
-
+                            
+                            // So the way to draw table in the console is not that hard. We just need to use the string.format output by microsoft.
                             Console.WriteLine(
                                 "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                             Console.WriteLine(
@@ -177,7 +217,8 @@ namespace ConsoleApplication1
                                 "History Grade", "Total Grade", "Status");
                             Console.WriteLine(
                                 "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-
+                            
+                            // In here we calculate the status of the student.
                             for (int i = 0; i < data[number1 - 1].Count; i++)
                             {
                                 string getStatus = "";
@@ -220,6 +261,7 @@ namespace ConsoleApplication1
                     }
                     else if (number == 3)
                     {
+                        // Option 3 is the remove student.
                         Console.WriteLine("-----------------------------");
                         if (data.Count == 0)
                         {
@@ -292,13 +334,14 @@ namespace ConsoleApplication1
                                 Console.WriteLine(
                                     "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                             }
-
+                            
+                            // We input to remove a specific student. (1)
                             Console.Write("Input to remove: ");
                             int index = int.Parse(Console.ReadLine());
 
-                            if (index > 0 && index <= data[number1 - 1].Count)
+                            if (index > 0 && index <= data[number1 - 1].Count) // (1)
                             {
-                                if (index == data[number1 - 1].Count && data[number1 - 1].Count == 1)
+                                if (index == data[number1 - 1].Count && data[number1 - 1].Count == 1) // (1)
                                 {
                                     data.RemoveAt(index - 1);
                                 }
@@ -323,6 +366,7 @@ namespace ConsoleApplication1
                     }
                     else if (number == 4)
                     {
+                        // Option 4 is used to remove class
                         Console.WriteLine("-----------------------------");
                         if (data.Count == 0)
                         {
@@ -335,7 +379,9 @@ namespace ConsoleApplication1
                         {
                             Console.WriteLine($"-> {i + 1}. {data[i][0][3]}");
                         }
-
+                        
+                        // Like section number 3: remove student.
+                        // Here we will remove a specific class out of the database.
                         Console.Write("Input to use: ");
                         int number1 = int.Parse(Console.ReadLine());
 
@@ -354,6 +400,7 @@ namespace ConsoleApplication1
                     }
                     else if (number == 5)
                     {
+                        // Option 5 is used to edit student's data.
                         Console.WriteLine("-----------------------------");
                         if (data.Count == 0)
                         {
@@ -426,10 +473,12 @@ namespace ConsoleApplication1
                                 Console.WriteLine(
                                     "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                             }
-
+                            
+                            // We accept the user input as an integer.
                             Console.Write("Input to edit: ");
                             int index = int.Parse(Console.ReadLine());
-
+                            
+                            // The titleInfo array is just for custom output
                             string[] titleInfo =
                             {
                                 "IDs", "Name", "Age", "Class", "Math Grade", "Physics Grade", "Chemistry Grade",

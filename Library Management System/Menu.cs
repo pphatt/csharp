6c4e9ab48a6e120cd117fe59d546f750195e48a9
phatshibukai <vutienphat0809@gmail.com>
2022-06-13 23:05:29 +0700

    Update: Delete book

--- Library Management System/Menu.cs
+++ Library Management System/Menu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Library_Management_System
@@ -7,11 +8,28 @@ namespace Library_Management_System
     {
         BookList book = new BookList();
 
-        private void CustomOutput(string option, string[] data, int index)
+        private void CustomOutput(string option, string[] data, string name, int index)
         {
             int j = 0;
-            Console.Write($"Input {option} to search: ");
+            bool check = false;
+            Console.Write($"Input {name} to search: ");
             option = Console.ReadLine();
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                string[] output1 = data[i].Split(',');
+                if (output1[index] == option)
+                {
+                    check = true;
+                }
+            }
+            
+            if (check == false)
+            {
+                Console.Write($"\t\t\t\t\t\tThere is no {option} in {name} section in the database\t\t\t\t\t\t");
+                return;
+            }
+
             Console.WriteLine(
                 "------------------------------------------------------------------------------------------------------------------------------------------------------");
             Console.WriteLine($"|{"ID",-4}|{"Name",-60}|{"Author",-40}|{"Category",-20}|{"Date",-20}|");
@@ -30,6 +48,8 @@ namespace Library_Management_System
                     j++;
                 }
             }
+
+            Console.ReadKey();
         }
 
         public Menu()
@@ -67,6 +87,7 @@ namespace Library_Management_System
                         Console.WriteLine("\t\t\t\t\t\t** 3. SEARCH BY AUTHOR                  **\t\t\t\t\t");
                         Console.WriteLine("\t\t\t\t\t\t** 4. SEARCH BY CATEGORY                **\t\t\t\t\t");
                         Console.WriteLine("\t\t\t\t\t\t** 5. SEARCH BY DATE                    **\t\t\t\t\t");
+                        Console.WriteLine("\t\t\t\t\t\t** 6. EXIT                              **\t\t\t\t\t");
                         Console.WriteLine("\t\t\t\t\t\t******************************************\t\t\t\t\t");
                         Console.Write("Input to use: ");
                         int number1 = int.Parse(Console.ReadLine());
@@ -89,16 +110,18 @@ namespace Library_Management_System
                                     "------------------------------------------------------------------------------------------------------------------------------------------------------");
                                 break;
                             case 2:
-                                CustomOutput("title", data, 0);
+                                CustomOutput("title", data, "title",0);
                                 break;
                             case 3:
-                                CustomOutput("author", data, 1);
+                                CustomOutput("author", data, "author",1);
                                 break;
                             case 4:
-                                CustomOutput("category", data, 2);
+                                CustomOutput("category", data, "category",2);
                                 break;
                             case 5:
-                                CustomOutput("date", data, 3);
+                                CustomOutput("date", data, "date",3);
+                                break;
+                            case 6:
                                 break;
                             default:
                                 Console.WriteLine("Invalid number");
@@ -109,7 +132,52 @@ namespace Library_Management_System
                     {
                         Console.WriteLine("There are no data currently");
                     }
-                } else if (number == 5)
+                } else if (number == 3)
+                {
+                    string[] data = File.ReadAllLines(@"D:\Dev\School\Library Management System\MyTest.txt");
+                    Console.WriteLine("\t\t\t\t\t\t****************** MENU ******************\t\t\t\t\t");
+                    Console.WriteLine("\t\t\t\t\t\t** 1. DELETE BY ID                      **\t\t\t\t\t");
+                    Console.WriteLine("\t\t\t\t\t\t** 2. DELETE BY NAME                    **\t\t\t\t\t");
+                    Console.WriteLine("\t\t\t\t\t\t******************************************\t\t\t\t\t");
+                    Console.Write("Input to use: ");
+                    int number1 = int.Parse(Console.ReadLine());
+
+                    switch (number1)
+                    {
+                        case 1:
+                            Console.Write("Input ID to delete: ");
+                            int number2 = int.Parse(Console.ReadLine());
+                            
+                            List<string> output = new List<string>(data);
+                            output.RemoveAt(number2 - 1);
+                            File.WriteAllLines(@"D:\Dev\School\Library Management System\MyTest.txt", output.ToArray());
+                            Console.WriteLine("\t\t\t\t\t\t*********** DELETE SUCCESSFULLY **********\t\t\t\t\t");
+                            
+                            break;
+                        case 2:
+                            Console.Write("Input Name to delete: ");
+                            string name = Console.ReadLine();
+                            List<string> output1 = new List<string>(data);
+                            
+                            for (int i = 0; i < data.Length; i++)
+                            {
+                                string[] output_ = data[i].Split(',');
+                                if (output_[0] == name)
+                                {
+                                    output1.RemoveAt(i);
+                                    i--;
+                                }
+                            }
+                            File.WriteAllLines(@"D:\Dev\School\Library Management System\MyTest.txt", output1.ToArray());
+                            Console.WriteLine("\t\t\t\t\t\t*********** DELETE SUCCESSFULLY **********\t\t\t\t\t");
+                            
+                            break;
+                        default:
+                            Console.WriteLine("Invalid number");
+                            break;
+                    }
+                }
+                else if (number == 5)
                 {
                     book.Show();
                     Console.ReadKey();
